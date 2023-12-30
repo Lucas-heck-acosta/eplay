1b29da92ec7afba931358174c50ff3e3211db10f
Lucas Acosta <lucas.heck@live.com>
2023-12-30 19:05:34 -0300

    add header responsiveness

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,19 +1,21 @@
 import styled from 'styled-components'
 import { breakpoints, cores } from '../../styles'
 
+export const Links = styled.ul`
+  display: flex;
+  margin-left: 40px;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    margin-left: 0;
+    display: block;
+  }
+`
+
 export const HeaderBar = styled.header`
   background-color: ${cores.cinza};
   padding: 24px;
   border-radius: 16px;
   margin-bottom: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  div {
-    display: flex;
-    align-items: center;
-  }
 
   a {
     color: ${cores.branca};
@@ -22,16 +24,28 @@ export const HeaderBar = styled.header`
   }
 
   @media (max-width: ${breakpoints.tablet}) {
-    display: none;
   }
 `
 
-export const Links = styled.ul`
-  display: flex;
-  margin-left: 40px;
+export const NavMobile = styled.nav`
+  display: none;
+
+  &.is-open {
+    display: block;
+  }
 `
+
 export const LinkItem = styled.li`
   margin-right: 16px;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    margin-right: 0;
+    a {
+      display: block;
+      padding: 16px 0;
+      text-align: center;
+    }
+  }
 `
 
 export const CartButton = styled.a`
@@ -40,4 +54,44 @@ export const CartButton = styled.a`
     margin-left: 16px;
   }
   cursor: pointer;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    span {
+      display: none;
+    }
+  }
+`
+
+export const Hamburguer = styled.div`
+  width: 32px;
+  span {
+    height: 2px;
+    display: block;
+    width: 100%;
+    background-color: ${cores.branca};
+    margin-bottom: 4px;
+  }
+
+  @media (min-width: ${breakpoints.desktop}) {
+    display: none;
+  }
+`
+
+export const HeaderRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    @media (max-width: ${breakpoints.desktop}) {
+      flex: 1;
+      justify-content: space-between;
+      ${Links} {
+        display: none;
+      }
+    }
+  }
 `
